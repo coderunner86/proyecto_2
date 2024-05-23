5e6d863ba66100041b10233e45e6b0314fe695c3
Bin Bash <java@unicauca.edu.co>
2024-05-23 14:35:35 -0500

    chore: Update

--- main.py
+++ main.py
@@ -5,8 +5,15 @@ from utils.process import rename_columns, process_dataframe
 from utils.filter import filter_by_column
 from utils.export import export_data
 
+"""
+Este módulo principal descarga, procesa, filtra y exporta datos obtenidos de la url o de un archivo local csv.
+"""
+
 def main():
-    url = "https://www.datos.gov.co/api/views/yhxn-eqqw/rows.csv?accessType=DOWNLOAD"
+    
+    url = (
+        "https://www.datos.gov.co/api/views/yhxn-eqqw/rows.csv?accessType=DOWNLOAD"
+    )
     csv_text = download_data(url)
 
     local_file_path = os.path.join('data', 'data.csv')
@@ -26,33 +33,63 @@ def main():
         print("1. Preprocesar la data")
         print("2. Filtrar por género")
         print("3. Filtrar por cualquier otra columna")
-        print("4. Mostrar conteo de víctimas por año, mes, departamento, actividad, género, estado")
+        print("4. Mostrar conteo de víctimas por año, mes, departamento, actividad, género o estado")
         print("5. Salir")
         option = input("Seleccione una opción: ")
 
         if option == '1':
+            
             df = rename_columns(df, {'genero': 'estado', 'estado': 'genero'})
             df = process_dataframe(df)
-            column_order = ['genero', 'rangoedad', 'estado', 'Actividad', 'condicion', 'grupoetnico',
-                            'tipoevento', 'Ubicación', 'municipio', 'departamento', 'sitio', 'latitudcabecera',
-                            'longitudcabecera', 'tipoarea', 'ano', 'mes', 'codigodanemunicipio', 'codigodanedepartamento']
+            
+            column_order = [
+                'genero', 'rangoedad', 'estado', 'Actividad', 'condicion', 
+                'grupoetnico', 'tipoevento', 'Ubicación', 'municipio', 
+                'departamento', 'sitio', 'latitudcabecera', 
+                'longitudcabecera', 'tipoarea', 'ano', 'mes', 
+                'codigodanemunicipio', 'codigodanedepartamento'
+            ]
+            
             df = df.reindex(columns=column_order)
             export_data(df, 'processed_data.csv')
             print("Los datos han sido limpiados y exportados.")
 
         elif option == '2':
-            genre_user = input("Ingresa el género (Hombre o Mujer): ").capitalize()
+            
+            genre_user = input(
+            """Ingresar género (Hombre o Mujer): """
+            ).capitalize()
+            
             df_filtered = df.loc[df['genero'] == genre_user]
-            export_data(df_filtered, f'genero_{genre_user}.csv')
+            
+            export_data(
+                df_filtered, 
+                f'genero_{genre_user}.csv'
+            )
             print(df_filtered.tail())
 
         elif option == '3':
-            column_name = input("Ingresa el nombre de la columna para filtrar: ").lower()
-            value = input(f"Ingresa un valor de dato de la columna {column_name} por el que quieras filtrar: ")
+            
+            column_name = input(
+            """Ingresa el nombre de la columna para filtrar: """
+            ).lower()
+
+            value = input(
+            f"""Ingresa un valor de {column_name} para filtrar: """
+            )
+            
             df_filtered = filter_by_column(df, column_name, value)
             print(df_filtered)
-            export_data(df_filtered, f'filtered_by_{column_name}_{value}.csv')
-            print(f"Datos filtrados por '{column_name}' = '{value}' exportados a 'data/filtered_by_{column_name}_{value}.csv'.")
+
+            export_data(
+                df_filtered, 
+                f"""filtered_by_{column_name}_{value}.csv"""
+            )
+
+            print(
+                f"""Datos filtrados por '{column_name}' = '{value}' 
+                exportados a 'data/filtered_by_{column_name}_{value}.csv'."""
+                )
 
             print(df.head())
 
