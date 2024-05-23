3dffbe671990541d21f341003fec95e63c02873a
Bin Bash <java@unicauca.edu.co>
2024-05-23 15:16:14 -0500

    fix: Remove typo

--- utils/download.py
+++ utils/download.py
@@ -5,7 +5,7 @@ def download_data(url, timeout=10):
     """Descarga datos desde una URL específica y retorna el contenido en texto."""
     try:
         response = requests.get(url, timeout=timeout)
-        response=raise_for_status()
+        response.raise_for_status()
         return response.text
     except requests.exceptions.RequestException as e:
         print(f"Error al descargar el archivo: {e}")
