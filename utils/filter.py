7ae55a072cdccbd647cca671a409597493da2dc6
Bin Bash <java@unicauca.edu.co>
2024-05-23 15:35:12 -0500

    refactor: Improve readability of the filter module

--- utils/filter.py
+++ utils/filter.py
@@ -1,3 +1,5 @@
+"""Este módulo contiene funciones para filtrar DataFrames basados en el valor de una columna."""
+
 import pandas as pd
 from utils.process import normalize_text
 
@@ -10,6 +12,6 @@ def filter_by_column(df, column_name, value):
         normalized_value = normalize_text(value)
         df[actual_column_name] = df[actual_column_name].apply(normalize_text)
         return df.loc[df[actual_column_name] == normalized_value]
-    else:
-        print(f"Columna '{column_name}' no encontrada en el DataFrame.")
-        return pd.DataFrame()
+
+    print(f"Columna '{column_name}' no encontrada en el DataFrame.")
+    return pd.DataFrame()
